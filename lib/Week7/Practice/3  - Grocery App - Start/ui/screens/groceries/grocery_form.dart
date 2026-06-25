c320ef095e3a38f750296f8a2975ab7fca954cf0
mengyeanuch <mengyeanuch168168@gmail.com>
2026-06-26 02:09:13 +0700

    Fixed Grocery App

--- lib/Week7/Practice/3  - Grocery App - Start/ui/screens/groceries/grocery_form.dart
+++ lib/Week7/Practice/3  - Grocery App - Start/ui/screens/groceries/grocery_form.dart
@@ -1,14 +1,122 @@
-
 // ---------------------------------------------
 // Create a new statefull widget : GroceryForm
 // ---------------------------------------------
-  
+
 // The form shall be composed of 2 text fields:
 // -	Name of the grocery item
- //-	Quantity (number only)
+//-	Quantity (number only)
 
 // ⚠️  For now we don’t select the grocery type, we assume it’s always food
 
 // The form shall be composed of 2 buttons:
 //-	Cancel button
 // -	Add item button
+import 'package:flutter/material.dart';
+import '../../../models/grocery.dart';
+
+class GroceryForm extends StatefulWidget {
+  const GroceryForm({super.key});
+
+  @override
+  State<GroceryForm> createState() => _GroceryFormState();
+}
+
+class _GroceryFormState extends State<GroceryForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _quantityController.dispose();
+    super.dispose();
+  }
+
+  void _reset(){
+    _nameController.clear();
+    _quantityController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+   return Padding(
+      padding: EdgeInsets.fromLTRB(
+        16,
+        16,
+        16,
+        MediaQuery.of(context).viewInsets.bottom + 16,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(padding: const EdgeInsets.only(bottom: 16),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Add a new item',
+                    style: TextStyle(
+                      fontSize: 20,
+
+                    ),
+                  ),
+                ]
+                    
+              ),
+            ),
+            
+            TextField(
+              controller: _nameController,
+              maxLength: 50,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+
+            const SizedBox(height: 10),
+
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _quantityController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: 'Quantity'),
+                  ),
+                ),
+
+              ],
+            ),
+
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: _reset, child: const Text("Reset")),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    final grocery = GroceryItem(
+                      id: DateTime.now().toString(),
+                      name: _nameController.text,
+                      quantity: int.parse(_quantityController.text),
+                      category: GroceryCategory.fruit,
+                    );
+                    Navigator.pop(context,grocery);
+                  },
+                  child: const Text("Add Item"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Week7/Practice/3  - Grocery App - Start/ui/screens/groceries/grocery_screen.dart
+++ lib/Week7/Practice/3  - Grocery App - Start/ui/screens/groceries/grocery_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dart/Week7/Practice/3%20%20-%20Grocery%20App%20-%20Start/models/grocery.dart';
+import 'package:flutter_dart/Week7/Practice/3%20%20-%20Grocery%20App%20-%20Start/ui/screens/groceries/grocery_form.dart';
+import 'package:flutter_dart/Week7/Practice/3%20%20-%20Grocery%20App%20-%20Start/ui/screens/groceries/grocery_tile.dart';
 import '../../../data/mock_grocery_data.dart';
 
 class GroceryScreen extends StatefulWidget {
@@ -9,13 +12,21 @@ class GroceryScreen extends StatefulWidget {
 }
 
 class _GroceryScreenState extends State<GroceryScreen> {
-  void onCreate() {
-    
     // ---------------------------------------------
     // Navigate to the form screen using showModalBottomSheet
     // ---------------------------------------------
 
     // https://api.flutter.dev/flutter/material/showModalBottomSheet.html
+  //   void onCreate() {
+  //   showModalBottomSheet(context: context, builder: (ctx) => const GroceryForm());
+  // } change to future
+  Future<void> onCreate() async{
+    final GroceryItem? grocery = await showModalBottomSheet(context: context, builder: (ctx) => const GroceryForm());
+    if (grocery == null) return;
+
+    setState(() {
+      allGroceryItems.add(grocery);
+    });
   }
 
   @override
@@ -28,12 +39,23 @@ class _GroceryScreenState extends State<GroceryScreen> {
       //  For each grocery items, create a GroceryTile (grocery_tile.dart)
       // ---------------------------------------------
       // https://api.flutter.dev/flutter/widgets/ListView-class.html
+      content=ListView.builder(
+        itemCount: allGroceryItems.length,
+        itemBuilder: (context, index) {
+          return GroceryTile(
+            grocery: allGroceryItems[index]
+          );
+        },
+      
+      );
     }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
-        actions: [IconButton(onPressed: () => {}, icon: const Icon(Icons.add))],
+        actions: [IconButton(
+          onPressed: onCreate,//() => {},
+          icon: const Icon(Icons.add))],
       ),
       body: content,
     );
